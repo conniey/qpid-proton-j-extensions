ca88dbd59e8ffab11cdbe80a0fcd964fd4338997
Connie <conniey@microsoft.com>
2019-08-21 18:47:25 -0700

    Add new line to StringUtils.

--- src/main/java/com/microsoft/azure/proton/transport/proxy/impl/StringUtils.java
+++ src/main/java/com/microsoft/azure/proton/transport/proxy/impl/StringUtils.java
@@ -1,6 +1,8 @@
 package com.microsoft.azure.proton.transport.proxy.impl;
 
 class StringUtils {
+    static final String NEW_LINE = "\r\n";
+
     static boolean isNullOrEmpty(String string) {
         return string == null || string.isEmpty();
     }
